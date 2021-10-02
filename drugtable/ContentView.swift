43ef23f15c658b06bec6b6d7a8b4dcaa5a3328bd
vasbaz <vasilchenko-bazarov@yandex.ru>
2021-10-03 00:12:16 +0300

    Initial Commit

---
+++ drugtable/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  drugtable
+//
+//  Created by Danila Vasilchenko-Bazarov on 03.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ drugtable/drugtableApp.swift
@@ -0,0 +1,17 @@
+//
+//  drugtableApp.swift
+//  drugtable
+//
+//  Created by Danila Vasilchenko-Bazarov on 03.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct drugtableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
