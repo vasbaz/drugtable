7c50b2a80fd4e40dfd7fb9ff021914c3204cf0bc
vasbaz <vasilchenko-bazarov@yandex.ru>
2021-10-14 22:10:53 +0300

    Fixed build

--- Drugtable/Mocks/RemindersGroupViewMockProvider.swift
+++ Drugtable/Mocks/RemindersGroupViewMockProvider.swift
@@ -5,18 +5,16 @@
 //  Created by Danila Vasilchenko-Bazarov on 14.10.2021.
 //
 
-import Foundation
-
-class RemindersGroupViewMockProvider: MockProvider {
-    let takingTime: Interval;
-    let reminders: [Reminder]
-    
-    init(takingTime: Interval?) {
-        self.takingTime = takingTime ?? IntervalMockProvider().getMock()
-        self.reminders = RemindersArrayMockProvider().getMock()
-    }
-    
-    func getMock() -> RemindersGroupViewModel {
-        
-    }
-}
+//import Foundation
+//
+//class RemindersGroupViewMockProvider: MockProvider {
+//    let takingTime: Interval;
+//    let reminders: [Reminder]
+//
+//    init(takingTime: Interval?) {
+//        self.takingTime = takingTime ?? IntervalMockProvider().getMock()
+//        self.reminders = RemindersArrayMockProvider().getMock()
+//    }
+//
+//    func getMock() -> RemindersGroupViewModel {}
+//}
